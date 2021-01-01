c0ac2dacd07e470e785721218b2fa22f76d55f8d
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-01-01 14:41:22 +0530

    importing other files to our file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,7 @@
-const fs = require('fs');
-fs.writeFileSync('notes.txt','this fule was created using node.js\n');
-fs.appendFileSync('notes.txt','now i am appending to this file using appendfile method of file system\n');
-fs.appendFileSync('notes.txt','Actua;;y i can append as many times i want\n');
-fs.appendFileSync('notes.txt','Each time i append new file wont be created and existing data wont be overridden\n');
+// const fs = require('fs');
+// fs.writeFileSync('notes.txt','this fule was created using node.js\n');
+// fs.appendFileSync('notes.txt','now i am appending to this file using appendfile method of file system\n');
+// fs.appendFileSync('notes.txt','Actua;;y i can append as many times i want\n');
+// fs.appendFileSync('notes.txt','Each time i append new file wont be created and existing data wont be overridden\n');
+const a=require('./utils')
+console.log('app.js')
---
+++ notes-app/utils.js
@@ -0,0 +1,2 @@
+const name='Manjunatha';
+console.log(name);
