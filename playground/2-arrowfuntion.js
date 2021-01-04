353aa05bcf1d6d0a4228cc5bf3492bfdbc0c59c3
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-01-04 17:31:16 +0530

    shortened the method

--- playground/2-arrowfuntion.js
+++ playground/2-arrowfuntion.js
@@ -6,10 +6,17 @@
 // }
 // const square=(x)=>x*x
 // console.log(square(3590))
-const event={
-    name:'birthday party',
-    printGuestlist:function(){
-        console.log('guest list for bday partiess',this.name)
+// const event={
+//     name:'birthday party',
+//     printGuestlist:function(){
+//         console.log('guest list for bday partiess',this.name)
+//     }
+// }
+// event.printGuestlist()
+const event = {
+    name: 'birthday party',
+    printGuestlist() {
+        console.log('guest list for bday partiess', this.name)
     }
 }
 event.printGuestlist()
