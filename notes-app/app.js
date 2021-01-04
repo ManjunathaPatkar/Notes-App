34a3fdab09846e558d1bc597e4ae2b4345fc3a09
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-01-04 11:52:26 +0530

    getting input from command and adding it in text file

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,7 +4,8 @@
 // fs.appendFileSync('notes.txt','Actua;;y i can append as many times i want\n');
 // fs.appendFileSync('notes.txt','Each time i append new file wont be created and existing data wont be overridden\n');
 const a=require('./notes');
-const validator=require('validator');
+const fs=require('fs');
+// const validator=require('validator');
 const chalk=require('chalk');
 console.log(a());
 // console.log(validator.isEmail('g@gilom.'));
@@ -14,4 +15,12 @@ console.log(chalk.green('success!!'));
 // console.log(chalk.yellow('processing'));
 // console.log(chalk.red.bgCyanBright.bold('fail'));
 // console.log(chalk.red.inverse.bgCyan('Lol'));
-console.log(process.argv[2]);
+// console.log(process.argv[2]);
+// fs.writeFileSync('notes.txt','Notes');
+const command=process.argv[2];
+// fs.appendFileSync('notes.txt', '\n');
+if (command==='add'){
+    fs.appendFileSync('notes.txt',process.argv[3]);
+    fs.appendFileSync('notes.txt', '\n');
+    console.log('adding note');
+}
