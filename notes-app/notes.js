b7cdd636c4c81c5f59e16b44490ef3c44838d914
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-01-04 15:17:00 +0530

    added methods to read data to notes.json file

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,10 +1,33 @@
+const fs=require('fs');
 const getNotes=function(){
     return "Your notes...";
 }
 const addNotes=function(title,body){
-    console.log(title)
-    console.log(body)
+    // console.log(title)
+    // console.log(body)
+    const notes=loadNotes();
+    // console.log(notes)
+    notes.push({
+        title:title,
+        body:body
+    })
+    savenotes(notes)
+
 }
+const loadNotes=function(){
+    try{
+         const data=fs.readFileSync('notes.json')
+         const datajson=data.toString()
+         return JSON.parse(datajson)
+    }
+    catch(err){
+        return []
+    }
+}
+const savenotes=function(notes){
+    const newdata=JSON.stringify(notes)
+    fs.writeFileSync('notes.json',newdata)
+    }
 module.exports={
     getNotes:getNotes,
     addNotes:addNotes
