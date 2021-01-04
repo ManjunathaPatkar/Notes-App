12bdb0475c7cb3dd57cc7363a7667a5527aa3a34
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-01-04 17:22:22 +0530

    3rd type of arrow fun

--- playground/2-arrowfuntion.js
+++ playground/2-arrowfuntion.js
@@ -1,7 +1,8 @@
 // const square=function (x){
 //     return x*x;
 // }
-const square=(x)=>{
-    return x*x
-}
-console.log(square(354367890))
+// const square=(x)=>{
+//     return x*x
+// }
+const square=(x)=>x*x
+console.log(square(3590))
